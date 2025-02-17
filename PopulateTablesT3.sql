73692d036d11a42632cc82df00507c6e27d45e52
dhruvgenius <dhruvg03@outlook.com>
2025-02-17 18:53:06 -0500

    Created the Orders Table

--- PopulateTablesT3.sql
+++ PopulateTablesT3.sql
@@ -95,7 +95,7 @@ CREATE TABLE IF NOT EXISTS Applications (
     CompanyID INT NULL,  -- Only applicable for sponsor applications
     ApplicationStatus ENUM('Pending', 'Approved', 'Rejected') DEFAULT 'Pending',
     SubmissionDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    
+
     -- Review Columns
     ReviewedByAdminID INT NULL,  -- Used when a sponsor application is approved by an admin
     ReviewedBySponsorID INT NULL,  -- Used when a driver application is approved by a sponsor
@@ -105,4 +105,15 @@ CREATE TABLE IF NOT EXISTS Applications (
     FOREIGN KEY (CompanyID) REFERENCES Sponsor(CompanyID) ON DELETE SET NULL,
     FOREIGN KEY (ReviewedByAdminID) REFERENCES Admin(AdminID) ON DELETE SET NULL,
     FOREIGN KEY (ReviewedBySponsorID) REFERENCES SponsorUser(SponsorUserID) ON DELETE SET NULL
+);
+
+-- Orders Table (For Drivers and Sponsors)
+CREATE TABLE IF NOT EXISTS Orders (
+    OrderID INT AUTO_INCREMENT PRIMARY KEY,
+    DriverID INT,
+    SponsorID INT,
+    OrderDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PointsSpent INT NOT NULL
+    FOREIGN KEY (DriverID) REFERENCES Driver(DRIVERID) ON DELETE CASCADE,
+    FOREIGN KEY (SponsorID) REFERENCES Sponsor(CompanyID) ON DELETE CASCADE
 );
