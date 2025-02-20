66e1141c0c5abfa937a810e5d41426248e496c14
Will Watkins <138529915+thewillw3@users.noreply.github.com>
2025-02-20 18:50:54 -0500

    Reformatting About page.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -22,17 +22,29 @@ export default function About() {
     // Returning all necessary data from the database.
     // Additional logic is in case the data isn't fully loaded.
     return (
-        <div className="about-container">
-            <div className="starfield"/>
-            <div className="about-main">
-                <h1>AlienBaba — Safe Shuttling Starts Here</h1>
-                <div className="ship">
-                    <img src={ship} height="100px"/>
-                    <img src={flame} height="40px"/>
+        <div>
+            <div className="about-header">
+                <div className="starfield"/>
+                <div className="banner">
+                    <h1>AlienBaba — Safe Shuttling Starts Here</h1>
+                    <div className="ship">
+                        <img src={ship} height="100px"/>
+                        <img src={flame} height="40px"/>
+                    </div>
+                    <p className="team-info">Team {data ? data.team_number : "..."} &#9900; Version {data ? data.version_number : "..."} &#9900; {data ? data.release_date : "..."}</p>
                 </div>
-                <p className="team-info">Team {data ? data.team_number : "..."} &#9900; Version {data ? data.version_number : "..."} &#9900; {data ? data.release_date : "..."}</p>
+                <div className="starfield" />
             </div>
-            <div className="starfield" />
+            <div className="info-container">
+                <h2>{data ? data.product_name : "..."}</h2>
+                <p>{data ? data.product_description : "..."}</p>
+                <ul>
+                    <li>Points System</li>
+                    <li>Rewards Catalog</li>
+                    <li>Responsive Applications</li>
+                    <li>Sponsor Controls</li>
+                </ul>
+            </div>
         </div>
     )
 }
